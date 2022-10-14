1f6f01210adbe31a0327d92795bde96ad67206c1
Hafiz ajani <apiatheking@hotmail.com>
2022-10-14 12:11:40 +0100

    Added some UI testlayers

---
+++ StaffsDirectory AppUITests/StaffsDirectoryUITestsLaunchTests.swift
@@ -0,0 +1,21 @@
+//
+//  StaffsDirectoryUITestsLaunchTests.swift
+//  StaffsDirectoryUITests
+//
+//  Created by Hafiz on 12/10/22.
+//
+
+import XCTest
+
+class StaffsDirectoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    
+}
